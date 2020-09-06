2d6a300029d5cd2a06c6f946387c5f3684632be2
dmitryman <dmitryman850@gmail.com>
2020-09-06 23:42:34 +0300

    deleted toast in ViewPager

--- app/src/main/java/com/example/finaltask/MainPageActivity.kt
+++ app/src/main/java/com/example/finaltask/MainPageActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -40,8 +39,8 @@ class MainPageActivity : AppCompatActivity(), RandomFragment.OnCurrentFragmentCl
             }
 
             override fun onPageSelected(position: Int) {
-                Toast.makeText(this@MainPageActivity, position.toString(), Toast.LENGTH_SHORT)
-                    .show()
+//                Toast.makeText(this@MainPageActivity, position.toString(), Toast.LENGTH_SHORT)
+//                    .show()
             }
         })
     }
